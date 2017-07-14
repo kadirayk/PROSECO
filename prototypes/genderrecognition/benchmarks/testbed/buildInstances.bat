dd9f9ea1f97c8255bb702c4e96f3f5d921f37ad4
mwever <wever@mail.uni-paderborn.de>
2017-07-14 16:01:22 +0200

    added buildInstances

---
+++ prototypes/genderrecognition/benchmarks/testbed/buildInstances.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d %~dp0
+java -cp ".;libs/weka.jar;libs/Catalano.Image.jar;libs/JAICore.jar;libs/commons-io-2.5.jar;libs/Catalano.Math.jar;libs/Catalano.Statistics.jar" GenderPredictor -i ../../data.zip %1
