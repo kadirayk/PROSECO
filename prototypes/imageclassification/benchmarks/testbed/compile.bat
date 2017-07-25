fdb5517de5a365cc1f5e56274999c2b51083da27
mwever <wever@mail.uni-paderborn.de>
2017-07-25 13:16:22 +0200

    Fixed bats to make ImageClassifier work

--- prototypes/imageclassification/benchmarks/testbed/compile.bat
+++ prototypes/imageclassification/benchmarks/testbed/compile.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d %~dp0
-javac -cp ".;libs/weka.jar;libs/util.jar;libs/JAICore.jar;libs/commons-io-2.5.jar;libs/Catalano.Image.jar" GenderPredictor.java
+javac -cp ".;libs/weka.jar;libs/util.jar;libs/JAICore.jar;libs/commons-io-2.5.jar;libs/Catalano.Image.jar" *.java
--- prototypes/imageclassification/benchmarks/testbed/test.bat
+++ prototypes/imageclassification/benchmarks/testbed/test.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d %~dp0
-java -cp ".;libs/weka.jar;libs/util.jar;libs/JAICore.jar;libs/Catalano.Image.jar;libs/Catalano.Math.jar;libs/Catalano.Statistics.jar" GenderPredictor -acc %1
+java -cp ".;libs/weka.jar;libs/util.jar;libs/JAICore.jar;libs/Catalano.Image.jar;libs/Catalano.Math.jar;libs/Catalano.Statistics.jar" ImageClassifier -acc %1
