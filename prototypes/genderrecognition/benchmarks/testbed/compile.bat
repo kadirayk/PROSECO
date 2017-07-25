175a276587b9ac8d5be04f2cc3b67ade722c7c98
mwever <wever@mail.uni-paderborn.de>
2017-07-25 09:49:51 +0200

    Abstracted genderrecognition prototype to imageclassifier that should be capable of several classes.

--- prototypes/genderrecognition/benchmarks/testbed/compile.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d %~dp0
-javac -cp ".;libs/weka.jar;libs/util.jar;libs/JAICore.jar;libs/commons-io-2.5.jar;libs/Catalano.Image.jar" GenderPredictor.java
--- prototypes/genderrecognition/initconfiguration.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-title Initial configuration routine
-echo starting initial configuration routine...
-java -jar InitConfiguration.jar
