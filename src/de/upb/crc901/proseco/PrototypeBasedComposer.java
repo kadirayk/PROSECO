54f46d20fa95c9e4292b01d3f51fadeb26496165
kadirayk <kadiray.k@hotmail.com>
2018-03-11 22:53:08 +0300

    refactored MovePlaceholderFilesToSource

--- src/de/upb/crc901/proseco/PrototypeBasedComposer.java
+++ src/de/upb/crc901/proseco/PrototypeBasedComposer.java
@@ -19,15 +19,15 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.upb.crc901.proseco.command.BootUpInternalBenchmarkServiceCommand;
 import de.upb.crc901.proseco.command.ExecuteStrategiesCommand;
 import de.upb.crc901.proseco.command.InitializeExecutionEnvironmentCommand;
-import de.upb.crc901.proseco.command.BootUpInternalBenchmarkServiceCommand;
+import de.upb.crc901.proseco.command.MovePlaceholderFilesToSourceCommand;
 import de.upb.crc901.proseco.command.ShutDownInternalBenchmarkServiceCommand;
 import de.upb.crc901.proseco.command.ValidatePrototypeCommand;
 import de.upb.crc901.proseco.command.WaitForStrategiesCommand;
 import de.upb.crc901.proseco.prototype.ExecutionEnvironment;
 import de.upb.crc901.proseco.util.Config;
-import jaicore.basic.FileUtil;
 import jaicore.basic.PerformanceLogger;
 
 public class PrototypeBasedComposer {
@@ -113,7 +113,8 @@ public class PrototypeBasedComposer {
 		PerformanceLogger.logEnd("shutdownInternalBenchmarkService");
 
 		PerformanceLogger.logStart("movePlaceholderFilesToSource");
-		this.movePlaceholderFilesToSource();
+		MovePlaceholderFilesToSourceCommand movePlaceholderFilesToSourceCommand = new MovePlaceholderFilesToSourceCommand(executionEnvironment);
+		movePlaceholderFilesToSourceCommand.execute();
 		PerformanceLogger.logEnd("movePlaceholderFilesToSource");
 
 		PerformanceLogger.logStart("executeGroundingRoutine");
@@ -229,67 +230,6 @@ public class PrototypeBasedComposer {
 			commandArgumentList.add(resource.getAbsolutePath());
 		}
 		return commandArgumentList;
-	}
-
-	/**
-	 * Move all placeholder files created by the winning strategy to the source
-	 * folder as inputs for the grounding routine.
-	 */
-	private void movePlaceholderFilesToSource() {
-		System.out.print("Copy placeholder files of winning strategies to src folder "
-				+ executionEnvironment.getGroundingDirectory() + " ...");
-
-		// Pick the output of the winning strategy
-		String winningStrategyName = "";
-		double fValue = 0.0;
-		for (final File strategy : executionEnvironment.getStrategyDirectory().listFiles()) {
-			if (!strategy.isDirectory()) {
-				continue;
-			}
-
-			final File fValueFile = new File(
-					strategy.getAbsolutePath() + File.separator + Config.OUTPUT_DIR + File.separator + "f.value");
-			if (!fValueFile.exists()) {
-				System.out.println(
-						"f.value file was not found for strategy; ignoring it: " + fValueFile.getAbsolutePath());
-				continue;
-			}
-			Double parsedValue = 0.0;
-			try {
-				parsedValue = Double.parseDouble(FileUtil.readFileAsString(fValueFile.getAbsolutePath()));
-			} catch (final NumberFormatException e) {
-				e.printStackTrace();
-			} catch (final IOException e) {
-				e.printStackTrace();
-			}
-
-			if (parsedValue >= fValue) {
-				winningStrategyName = strategy.getName();
-				fValue = parsedValue;
-			}
-		}
-
-		if (winningStrategyName.isEmpty()) {
-			throw new IllegalStateException("Name of the winning strategyg is not filled.");
-		}
-
-		final File winningStrategy = new File(executionEnvironment.getStrategyDirectory() + File.separator
-				+ winningStrategyName + File.separator + Config.OUTPUT_DIR);
-		for (final File strategyFile : winningStrategy.listFiles()) {
-			if (strategyFile.isFile()) {
-				final File groundingFolderFile = new File(
-						executionEnvironment.getGroundingDirectory() + File.separator + strategyFile.getName());
-				try {
-					FileUtils.copyFile(strategyFile, groundingFolderFile);
-				} catch (final IOException e) {
-					System.out.println("\nCould not move placeholder file " + strategyFile.getName() + " from "
-							+ strategyFile.getAbsolutePath() + " to " + groundingFolderFile.getAbsolutePath());
-					e.printStackTrace();
-				}
-			}
-		}
-
-		System.out.println("DONE.");
 	}
 
 	private void executeGroundingRoutine() {
