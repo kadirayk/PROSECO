ce035372ae53dff43c37e248675f42613ddb09e0
kadirayk <kadiray.k@hotmail.com>
2018-04-08 15:45:25 +0200

    can run game server via web ui

--- prototypes/automl/strategies/strategy1/run.bat
+++ prototypes/automl/strategies/strategy1/run.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d %~dp0
 title Dummy Strategy
-echo Starting execution of HTN Strategy...
+echo Starting execution of Strategy...
 java -jar strategy.jar ../../params/ output/ ../../benchmarks/benchmarkTaskOffer.bat
