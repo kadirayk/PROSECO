8011ff2a1efba57c609ef723a2519d28e39ac880
Felix Mohr <fmohr@mail.upb.de>
2018-09-20 10:30:31 +0200

    cleaned repository from eclipse specific stuff

--- prototypes/automl/strategies/strategy1/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-title Dummy Strategy
-echo Starting execution of Strategy...
-java -jar strategy.jar ../../params/ output/ ../../benchmarks/benchmarkTaskOffer.bat %~dp0
--- prototypes/imageclassification/strategies/htn/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-title HTN Strategy
-echo Starting execution of HTN Strategy...
-java -jar strategy.jar ../../params/ output/ ../../benchmarks/benchmarkTaskOffer.bat %~dp0
