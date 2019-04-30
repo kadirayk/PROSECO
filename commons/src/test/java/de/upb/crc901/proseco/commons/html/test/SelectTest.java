0b1c264a12efb3a69faa331342d6fb181546f302
kadirayk <kadiray.k@hotmail.com>
2019-04-30 13:56:55 +0200

    added test cases to commons module

---
+++ commons/src/test/java/de/upb/crc901/proseco/commons/html/test/SelectTest.java
@@ -0,0 +1,44 @@
+package de.upb.crc901.proseco.commons.html.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import de.upb.crc901.proseco.commons.html.Option;
+import de.upb.crc901.proseco.commons.html.Select;
+
+public class SelectTest {
+
+	@Test
+	public void toHTMLTest() {
+		Map<String, String> attributes = new HashMap<>();
+		attributes.put("name", "selectName");
+
+		List<Option> options = new ArrayList<>();
+
+		Map<String, String> option1Attributes = new HashMap<>();
+		option1Attributes.put("value", "option1");
+		options.add(new Option("Value1", option1Attributes));
+
+		Map<String, String> option2Attributes = new HashMap<>();
+		option2Attributes.put("value", "option2");
+		options.add(new Option("Value2", option2Attributes));
+
+		Select select = new Select(null, attributes, options);
+
+		String actual = select.toHTML();
+		String expected = "<select name=\"response\">"
+				+ "\n\t<option value=\"option1\">Value1</option>"
+				+ "\n\t<option value=\"option2\">Value2</option>"
+				+ "\n</select>";
+
+		assertEquals(expected, actual);
+
+	}
+
+}
