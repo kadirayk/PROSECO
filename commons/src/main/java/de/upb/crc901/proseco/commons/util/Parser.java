aced5c7dc2e85bef262d1d1d9ad697a519d555b3
kadirayk <kadiray.k@hotmail.com>
2019-05-22 19:15:43 +0200

    resolve code smells in Parser

--- commons/src/main/java/de/upb/crc901/proseco/commons/util/Parser.java
+++ commons/src/main/java/de/upb/crc901/proseco/commons/util/Parser.java
@@ -7,8 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
@@ -17,7 +15,6 @@ import de.upb.crc901.proseco.commons.interview.Interview;
 import de.upb.crc901.proseco.commons.interview.Question;
 import de.upb.crc901.proseco.commons.interview.QuestionCollection;
 import de.upb.crc901.proseco.commons.interview.State;
-import de.upb.crc901.proseco.commons.util.ListUtil;
 
 /**
  * Interview Parser utility
@@ -50,12 +47,10 @@ public class Parser {
 	 *
 	 * @param interviewFile
 	 * @return
-	 * @throws JsonParseException
-	 * @throws JsonMappingException
 	 * @throws IOException
 	 */
-	public Interview initializeInterviewFromConfig(File interviewFile) throws JsonParseException, JsonMappingException, IOException {
-		
+	public Interview initializeInterviewFromConfig(File interviewFile) throws IOException {
+
 		/* load and parse interview core file */
 		File interviewFolder = interviewFile.getParentFile();
 		Interview interview = null;
@@ -63,55 +58,58 @@ public class Parser {
 		interview = mapper.readValue(interviewFile, Interview.class);
 		if (interview == null)
 			throw new IllegalStateException("Interview parser returned NULL");
-		
+
 		/* retrieve questions specified in question repository (if any exists) */
 		String questionPath = interview.getQuestionRepo();
 		File questionFile = new File(interviewFolder + File.separator + questionPath);
 		QuestionCollection qCollection = parseQuestion(questionFile.getAbsolutePath());
-		
-		/* check whether questions of the interview file must be overwritten by those in the question file (iff the content is undefined) */
-//		Set<String> questionSet = new HashSet<>();
-//		interview.setQuestionSet(questionSet);
+
+		/*
+		 * check whether questions of the interview file must be overwritten by those in
+		 * the question file (iff the content is undefined)
+		 */
+		checkAndOverwriteQuestions(interview, qCollection);
+		setTimeOutQuestion(interview);
+		return interview;
+	}
+
+	private void checkAndOverwriteQuestions(Interview interview, QuestionCollection qCollection) {
 		if (qCollection != null) {
 			for (State s : interview.getStates()) {
 				List<Question> questions = s.getQuestions();
 				if (ListUtil.isNotEmpty(questions)) {
 					for (Question q : new ArrayList<>(questions)) {
-//						StringBuilder questionSetItem = new StringBuilder(s.getName());
-//						questionSetItem.append(".").append(q.getId());
-						if (q.getContent() == null) { // overwrite question by the question in the question file if no content is provided
+						if (q.getContent() == null) { // overwrite question by the question in the question file if no
+														// content is provided
 							questions.remove(q);
 							questions.add(qCollection.getQuestionById(q.getId()));
 						}
-//						questionSet.add(questionSetItem.toString());
 					}
 				}
 			}
-
 		}
-		setTimeOutQuestion(interview);
-		return interview;
 	}
 
 	private void setTimeOutQuestion(Interview interview) {
+		String timeout = "timeout";
 		List<State> states = interview.getStates();
 		State stateBeforeTimeout = states.get(states.size() - 2);
 		State stateAfterTimeout = states.get(states.size() - 1);
 		Map<String, String> transition = new HashMap<>();
-		transition.put("default", "timeout");
+		transition.put("default", timeout);
 		stateBeforeTimeout.setTransition(transition);
 		State timeOutState = new State();
-		timeOutState.setName("timeout");
+		timeOutState.setName(timeout);
 		Map<String, String> timeoutTransition = new HashMap<>();
 		timeoutTransition.put("default", stateAfterTimeout.getName());
 		timeOutState.setTransition(timeoutTransition);
 		List<Question> questions = new ArrayList<>();
 		Question q = new Question();
-		q.setId("timeout");
+		q.setId(timeout);
 		q.setContent("Please specify a time out value in seconds");
 		Input input = new Input();
 		Map<String, String> attributeMap = new HashMap<>();
-		attributeMap.put("name", "timeout");
+		attributeMap.put("name", timeout);
 		attributeMap.put("type", "number");
 		input.setAttributes(attributeMap);
 		q.setUiElement(input);
@@ -119,7 +117,7 @@ public class Parser {
 		timeOutState.setQuestions(questions);
 		states.add(timeOutState);
 		Map<String, State> stateMap = interview.getStateMap();
-		stateMap.put("timeout", timeOutState);
+		stateMap.put(timeout, timeOutState);
 	}
 
 }
