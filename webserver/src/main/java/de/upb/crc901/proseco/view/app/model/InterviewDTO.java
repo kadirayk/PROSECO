0039a1bff46cbedb99cb9a67ab36d335e1cd4d88
kadirayk <kadiray.k@hotmail.com>
2019-05-22 19:09:13 +0200

    resolve code smells in InterviewDTO

--- webserver/src/main/java/de/upb/crc901/proseco/view/app/model/InterviewDTO.java
+++ webserver/src/main/java/de/upb/crc901/proseco/view/app/model/InterviewDTO.java
@@ -82,8 +82,8 @@ public class InterviewDTO {
 	}
 
 	/**
-	 * Do not show submit button for questions without uiElements (i.e. pages
-	 * that do not require input)
+	 * Do not show submit button for questions without uiElements (i.e. pages that
+	 * do not require input)
 	 */
 	private void setShowSubmitValue() {
 		List<Question> questions = this.interviewFillout.getCurrentState().getQuestions();
@@ -114,7 +114,18 @@ public class InterviewDTO {
 		StringBuilder htmlElement = new StringBuilder();
 		htmlElement.append("<div id=\"debugBox\" ng-show=\"pac.showDebugTable();\">");
 		htmlElement.append(HTMLConstants.LINE_BREAK).append("Debug: ");
-		htmlElement.append("<table style=\"width: 100%\" border=\"1\"><tr><th>").append("State").append("</th><th>").append("qId").append("</th><th>").append("Question").append("</th><th>").append("answer").append("</th></tr>");
+		htmlElement.append("<table style=\"width: 100%\" border=\"1\">").append(HTMLConstants.TR_OPEN)
+				.append(HTMLConstants.TH_OPEN).append("State").append(HTMLConstants.TH_CLOSE)
+				.append(HTMLConstants.TH_OPEN).append("qId").append(HTMLConstants.TH_CLOSE)
+				.append(HTMLConstants.TH_OPEN).append("Question").append(HTMLConstants.TH_CLOSE)
+				.append(HTMLConstants.TH_OPEN).append("answer").append(HTMLConstants.TH_CLOSE)
+				.append(HTMLConstants.TR_CLOSE);
+		createStatesHTML(interviewFillout, htmlElement);
+		htmlElement.append("</table>").append("</div>");
+		this.debugHTML = htmlElement.toString();
+	}
+
+	private void createStatesHTML(final InterviewFillout interviewFillout, StringBuilder htmlElement) {
 		for (State state : interviewFillout.getInterview().getStates()) {
 			if (state.getName().equals("timeout")) {
 				continue;
@@ -123,30 +134,33 @@ public class InterviewDTO {
 			if (state.getName().equals(interviewFillout.getCurrentState().getName())) {
 				htmlElement.append(" class=\"currentState\" ");
 			}
-			htmlElement.append("><td rowspan=\"").append(state.getQuestions().size()).append("\">").append(state.getName()).append("</td>");
+			htmlElement.append("><td rowspan=\"").append(state.getQuestions().size()).append("\">")
+					.append(state.getName()).append(HTMLConstants.TD_CLOSE);
 
-			boolean isFirstLoop = false;
-			for (Question question : state.getQuestions()) {
-				if (isFirstLoop) {
-					htmlElement.append("<tr");
-					if (state.getName().equals(interviewFillout.getCurrentState().getName())) {
-						htmlElement.append(" class=\"currentState\" ");
-					}
-					htmlElement.append(">");
-				}
-				htmlElement.append("<td>").append(question.getId()).append("</td>");
-				htmlElement.append("<td>").append(question.getContent()).append("</td>");
-				htmlElement.append("<td>");
-				if (interviewFillout.getAnswers().containsKey(question.getId())) {
-					htmlElement.append(interviewFillout.getAnswers().get(question.getId()));
-				}
-				htmlElement.append("</td></tr>");
-				isFirstLoop = true;
-			}
+			createQuestionsHTML(interviewFillout, htmlElement, state);
 
 		}
-		htmlElement.append("</table>").append("</div>");
-		this.debugHTML = htmlElement.toString();
+	}
+
+	private void createQuestionsHTML(final InterviewFillout interviewFillout, StringBuilder htmlElement, State state) {
+		boolean isFirstLoop = false;
+		for (Question question : state.getQuestions()) {
+			if (isFirstLoop) {
+				htmlElement.append("<tr");
+				if (state.getName().equals(interviewFillout.getCurrentState().getName())) {
+					htmlElement.append(" class=\"currentState\" ");
+				}
+				htmlElement.append(">");
+			}
+			htmlElement.append(HTMLConstants.TD_OPEN).append(question.getId()).append(HTMLConstants.TD_CLOSE);
+			htmlElement.append(HTMLConstants.TD_OPEN).append(question.getContent()).append(HTMLConstants.TD_CLOSE);
+			htmlElement.append(HTMLConstants.TD_OPEN);
+			if (interviewFillout.getAnswers().containsKey(question.getId())) {
+				htmlElement.append(interviewFillout.getAnswers().get(question.getId()));
+			}
+			htmlElement.append(HTMLConstants.TD_CLOSE).append(HTMLConstants.TR_CLOSE);
+			isFirstLoop = true;
+		}
 	}
 
 	public boolean isShowConfigurationPane() {
@@ -183,7 +197,10 @@ public class InterviewDTO {
 
 	@Override
 	public String toString() {
-		return "InterviewDTO [processId=" + this.processId + ", content=" + this.content + ", interviewFillout=" + this.interviewFillout + ", interviewHTML=" + this.interviewHTML + ", debugHTML=" + this.debugHTML + ", showInterview="
-				+ this.showInterview + ", showSubmit=" + this.showSubmit + ", showConfigurationPane=" + this.showConfigurationPane + ", showConsole=" + this.showConsole + ", upload=" + this.upload + "]";
+		return "InterviewDTO [processId=" + this.processId + ", content=" + this.content + ", interviewFillout="
+				+ this.interviewFillout + ", interviewHTML=" + this.interviewHTML + ", debugHTML=" + this.debugHTML
+				+ ", showInterview=" + this.showInterview + ", showSubmit=" + this.showSubmit
+				+ ", showConfigurationPane=" + this.showConfigurationPane + ", showConsole=" + this.showConsole
+				+ ", upload=" + this.upload + "]";
 	}
 }
