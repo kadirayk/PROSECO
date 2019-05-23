7344336d6a23aad027c270e5bb1a930bdac41786
kadirayk <kadiray.k@hotmail.com>
2019-05-23 22:59:37 +0200

    add default scores

--- core/domains/test/prototypes/test3/strategies/strategy5/run.sh
+++ core/domains/test/prototypes/test3/strategies/strategy5/run.sh
@@ -4,6 +4,7 @@ echo param1:$1 >> $1/strategy5.out
 echo param2:$2 >> $1/strategy5.out
 echo param3:$3 >> $1/strategy5.out
 echo param4:$4 >> $1/strategy5.out
+echo 0.9 > $3/score
 cd "${0%/*}"
 javac --release 8 BadScoreButNoErrors.java
 java BadScoreButNoErrors $1 $2 $3 $4
