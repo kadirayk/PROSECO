cc59d941ff0480d5f250f4add85e9066acc0c706
kadirayk <kadiray.k@hotmail.com>
2019-05-23 18:20:16 +0200

    resolve remaining code smells in commons

--- commons/src/main/java/de/upb/crc901/proseco/commons/controller/CannotFixDomainInThisProcessException.java
+++ commons/src/main/java/de/upb/crc901/proseco/commons/controller/CannotFixDomainInThisProcessException.java
@@ -2,6 +2,6 @@ package de.upb.crc901.proseco.commons.controller;
 
 public class CannotFixDomainInThisProcessException extends Exception {
 	public CannotFixDomainInThisProcessException() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 }
