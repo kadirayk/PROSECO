4ecbe7fd5ba12474825273edb1f1d455c3e81394
kadirayk <kadiray.k@hotmail.com>
2019-05-23 23:10:25 +0200

    add default scores

--- core/domains/test/prototypes/test3/strategies/strategy1/run.sh
+++ core/domains/test/prototypes/test3/strategies/strategy1/run.sh
@@ -4,7 +4,6 @@ echo param1:$1 >> $1/strategy1.out
 echo param2:$2 >> $1/strategy1.out
 echo param3:$3 >> $1/strategy1.out
 echo param4:$4 >> $1/strategy1.out
-echo 0.01 > $3/score
 cd "${0%/*}"
 javac --release 8 ExhaustCPU.java
 java ExhaustCPU 30 $2 $3 $4
