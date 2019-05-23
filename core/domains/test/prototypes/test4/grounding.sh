79124e9d0df42b718742ada9404e34bbd4761191
kadirayk <kadiray.k@hotmail.com>
2019-05-23 23:04:59 +0200

    fix grounding.sh

--- core/domains/test/prototypes/test4/grounding.sh
+++ core/domains/test/prototypes/test4/grounding.sh
@@ -3,6 +3,6 @@ echo file:$0 >> ../../../../processes/$1/test.out
 echo param1:$1 >> ../../../../processes/$1/test.out
 echo param2:$2 >> ../../../../processes/$1/test.out
 echo param3:$3 >> ../../../../processes/$1/test.out
-cd $0
+cd "${0%/*}"
 javac --release 8 Grounding.java
 java Grounding $1 $2 $3
