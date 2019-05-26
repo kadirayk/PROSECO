60f592dd39f84371e5e3b1948fad5a7f1894f117
kadirayk <kadiray.k@hotmail.com>
2019-05-26 17:25:56 +0200

    check if prototype exists

--- commons/src/main/java/de/upb/crc901/proseco/commons/controller/DefaultPrototypeExtractor.java
+++ commons/src/main/java/de/upb/crc901/proseco/commons/controller/DefaultPrototypeExtractor.java
@@ -1,13 +1,12 @@
 package de.upb.crc901.proseco.commons.controller;
 
+import java.io.File;
 import java.util.Map;
 
 public class DefaultPrototypeExtractor implements IPrototypeExtractor {
 
 	@Override
 	public String getPrototype(String domain, Map<String, String> answers) throws PrototypeCouldNotBeExtractedException {
-		// TODO get available protoypes for domain and compare if detected prototype
-		// exist for domain
 		String prototype = null;
 		if (answers != null) {
 			prototype = answers.get("Please select prototype");
@@ -15,10 +14,23 @@ public class DefaultPrototypeExtractor implements IPrototypeExtractor {
 				prototype = answers.get("prototype");
 			}
 		}
-		if (prototype == null) {
+
+		if (prototype == null || !prototypeExists(domain, prototype)) {
 			throw new PrototypeCouldNotBeExtractedException();
 		}
 		return prototype;
+	}
+	
+	private static boolean prototypeExists(String domain, String prototype) {
+		boolean exists = false;
+		File prototypesFolder = new File("domains/" + domain + "/prototypes");
+		for (File file : prototypesFolder.listFiles()) {
+			if (file.getName().equalsIgnoreCase(prototype)) {
+				exists = true;
+				break;
+			}
+		}
+		return exists;
 	}
 
 }
