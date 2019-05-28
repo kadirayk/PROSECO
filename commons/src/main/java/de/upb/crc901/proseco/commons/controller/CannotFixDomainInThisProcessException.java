c152ea1f5f47d8c86768797c3353024322c4ae42
kadirayk <kadiray.k@hotmail.com>
2019-05-28 19:52:15 +0200

    no need for constructor

--- commons/src/main/java/de/upb/crc901/proseco/commons/controller/CannotFixDomainInThisProcessException.java
+++ commons/src/main/java/de/upb/crc901/proseco/commons/controller/CannotFixDomainInThisProcessException.java
@@ -12,10 +12,4 @@ public class CannotFixDomainInThisProcessException extends IllegalStateException
 	 */
 	private static final long serialVersionUID = -6471138052887650425L;
 
-	/**
-	 * Default constructor
-	 */
-	public CannotFixDomainInThisProcessException() {
-		super();
-	}
 }
